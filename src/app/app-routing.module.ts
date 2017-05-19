8ac52968622f13c1ec777c5cce3eb2173d2e9b7e
Ted Mader <tlmader.dev@gmail.com>
2017-05-18 19:04:01 -0500

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MusicComponent } from './music/music.component';
+import { PhotosComponent } from './photos/photos.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'music', component: MusicComponent },
+  { path: 'photos', component: PhotosComponent },
+  { path: 'projects', component: ProjectsComponent }
 ];
 
 @NgModule({
