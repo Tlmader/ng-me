f7de50cd2767601947b8d4f3f480353e7585912b
Ted Mader <ted.mader@ge.com>
2017-05-22 15:08:16 -0500

    Create NavLinks class

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NavLink } from './navlink';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  navLinks: NavLink[];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    this.navLinks = [
+      {
+        label: 'Home',
+        route: 'home'
+      },
+      {
+        label: 'Projects',
+        route: 'projects'
+      },
+      {
+        label: 'Photos',
+        route: 'photos'
+      },
+      {
+        label: 'Music',
+        route: 'music'
+      }
+    ]
+  }
 }
