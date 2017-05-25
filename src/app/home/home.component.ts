74c8247cda3cbe90c6c6e738ea8260cddc4d0642
Ted Mader <ted.mader@ge.com>
2017-05-25 13:07:49 -0500

    Update css for abouts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -20,7 +20,6 @@ export class HomeComponent implements OnInit {
     ];
     this.abouts = [
       {
-        title: 'Programming',
         content: 'I studied computer science at the University of New Orleans and develop software for GE Digital. My ' +
         'experience comes from Java programming and Agile software projects. I also enjoy web development and ' +
         'recently began studying AI programming.',
@@ -31,7 +30,6 @@ export class HomeComponent implements OnInit {
         }
       },
       {
-        title: 'Video Games',
         content: 'I am an avid gamer who also enjoys developing for video games. I have built small games with Unity ' +
         'and Unreal Engine, including a 3D action game with my game development class at UNO! My ' +
         'favorite video games include Guild Wars, Undertale, Dark Souls, and Minecraft.',
