8b43f6c725c7d47b9a15f107e375d17553120a87
Ted Mader <ted.mader@ge.com>
2017-05-30 10:55:59 -0500

    Update playlists data

--- src/app/music/music.component.ts
+++ src/app/music/music.component.ts
@@ -15,19 +15,39 @@ export class MusicComponent implements OnInit {
   ngOnInit() {
     this.playlists = [
       {
-        title: 'My 2017 – SoundCloud',
+        title: 'chill-2017',
+        url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/319604460&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true'
+      },
+      {
+        title: 'electronic-2017',
+        url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/319604320&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true'
+      },
+      {
+        title: 'house-2017',
+        url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/319604122&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true'
+      },
+      {
+        title: 'lowfi-2017',
+        url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/322558490&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true'
+      },
+      {
+        title: 'trap-2017',
+        url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/319604149&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true'
+      },
+      {
+        title: 'my-2017',
         url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/297172538&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true'
       },
       {
-        title: 'My 2016 – SoundCloud',
+        title: 'my-2016',
         url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/288248586&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true',
       },
       {
-        title: 'Sets & Mixes – SoundCloud',
+        title: 'Sets & Mixes',
         url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/116495333&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true',
       },
       {
-        title: 'My 2016 – Spotify',
+        title: 'My 2016 (Spotify)',
         url: 'https://open.spotify.com/embed/user/tlmader/playlist/3QGFzhkCFDhZhisEDYQvPW',
       },
     ];
