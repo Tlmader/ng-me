a9291e8691f5e2b7debe1db6b4c8815d2e9fe2d0
Ted Mader <ted.mader@ge.com>
2017-05-30 10:59:32 -0500

    Optimize imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { FlexLayoutModule } from "@angular/flex-layout";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import {
-  MdButtonModule, MdCardModule, MdToolbarModule, MdTabsModule, MdGridListModule,
-  MdListModule, MdSelectModule
-} from "@angular/material";
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdButtonModule, MdCardModule, MdToolbarModule, MdTabsModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
