0ac52717c95bc1ad28edba9a8c51d1a916e18454
Ted Mader <ted.mader@ge.com>
2017-08-26 14:51:15 -0500

    Fix layouts for iOS

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,13 +15,12 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.intros = [
-      'My name is Ted Mader, and I am a full-time software engineer starting my career in the incredible city of New Orleans.',
       'I guess you\'re here because you want to know more about me, right? I\'m not that great at talking about myself, but I\'ll do my best and add some pictures.'
     ];
     this.abouts = [
       {
         title: null,
-        description: 'I studied computer science at the University of New Orleans and develop software for GE Digital. My experience comes from Java programming and Agile software projects. I have spent time developing web applications using technologies like Spring, Angular, and Node. I also enjoy web development and recently began studying AI programming.',
+        description: 'I studied computer science at the University of New Orleans and develop software for GE Digital. My experience comes from Java programming and Agile software projects. My favorite language is JavaScript, and I have spent time building web applications with technologies like Angular, React, and Express.',
         image: 'assets/home/programming.png',
         links: [{
           label: 'GitHub',
@@ -50,7 +49,7 @@ export class HomeComponent implements OnInit {
       },
       {
         title: null,
-        description: 'Of course, I still love playing video games. I enjoy building and exploring with friends in Terraria and Minecraft, the planning involved in a game of Sid Meier\'s Civilization, and slaying powerful bosses in Dark Souls. Stardew Valley has been a lot of fun recently. I am also a huge fan of stories told by indie titles like Undertale, Bastion, and Transistor.',
+        description: 'I still love playing video games. My favorite titles include Terraria, Undertale, Transistor, and Dark Souls.',
         image: 'assets/home/terraria.jpg',
         links: [{
           label: 'Visit me on Steam!',
